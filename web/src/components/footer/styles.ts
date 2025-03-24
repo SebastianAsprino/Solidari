9808f36d8b330123453ad39aad1f4635810f78fa
Sebastian Asprino <sebastian@asprino.dev>
2025-03-23 23:21:32 -0500

    diseño del footer y estilos

---
+++ web/src/components/footer/styles.ts
@@ -0,0 +1,14 @@
+const footer:string = "footer sm:footer-horizontal bg-black text-neutral-content items-center p-4";
+const footerAside:string = "grid-flow-col items-center";
+const footerAsideSVG:string = "fill-current";
+
+
+
+
+const styles = {
+footer,
+footerAside,
+footerAsideSVG
+};
+
+export default styles;
