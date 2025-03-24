c7fd126d0c96667bf222fac69146b37a6ac3c744
Sebastian Asprino <sebastian@asprino.dev>
2025-03-23 23:17:20 -0500

    pagina principal

---
+++ web/src/components/pagina-principal/styles.ts
@@ -0,0 +1,18 @@
+const main:string = "flex h-screen overflow-hidden";
+const SectionIzquierdo:string = "w-1/2 h-full flex flex-col justify-center items-center relative overflow-hidden bg-black text-white p-[5%] box-border";
+const SectionIzquierdoH1:string ="text-[6vw] mb-[2%] leading-[1.3] mt-[-9%] font-bold";
+const SectionIzquierdoP:string ="text-[1.5vw] mb-[8%]";
+const SectionIzquierdoA:string ="bg-white text-black py-[2%] px-[5%] no-underline rounded text-[2vw] transition-all duration-300 hover:scale-110 hover:shadow-lg hover:brightness-90 hover:bg-gray-200";
+const SectionDerecho:string ="w-1/2 h-full bg-white relative";
+const styles = {
+	main,
+	SectionIzquierdo,
+	SectionIzquierdoH1,
+	SectionIzquierdoP,
+	SectionIzquierdoA,
+	SectionDerecho
+};
+
+export default styles;
+
+
