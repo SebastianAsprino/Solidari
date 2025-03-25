7f254ae9bb252867e26e48eec1950c55386b2650
Sebastian Asprino <sebastian@asprino.dev>
2025-03-25 16:26:26 -0500

    terminado el header d ela pagina del proceso

--- web/src/components/header/styles.ts
+++ web/src/components/header/styles.ts
@@ -1,6 +1,20 @@
-const SectionDerecho:string ="w-1/2 h-full bg-white relative";
-const styles = {
+const Nav:string ="w-full max-w-full bg-black px-8 py-6 flex justify-between items-center h-auto";
+const NavDiv1:string = "flex items-center";
+const NavDivA:string = "h-auto w-[10%] translate-x-[50%]";
+const NavDivASVG:string = "max-w-[100%] h-auto";
+const NavDiv2:string = "flex items-center justify-between gap-4 translate-x-[-50%]";
+const NavDiv2A:string = "h-auto w-[100%]";
+const NavDiv2ASVG:string = "max-w-[100%] h-auto";
 
+
+const styles = {
+ Nav,
+ NavDiv1,
+ NavDivA,
+ NavDivASVG,
+ NavDiv2,
+ NavDiv2A,
+ NavDiv2ASVG
 };
 
 export default styles;
