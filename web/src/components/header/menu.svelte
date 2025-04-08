7c28e1d1b20e3de3b2998d3f88cedfecc8382e28
Sebastian Asprino <sebastian@asprino.dev>
2025-04-08 11:35:22 -0500

    refactorizado el header - estilo para celualres ok -  estilo para pc ok - se eliminaron los logos de whasa e instagram

---
+++ web/src/components/header/menu.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import s from "./styles";
+	let panelOpen:boolean = false;
+	const togglePanel = () => {
+		panelOpen = !panelOpen;
+	};
+	const closePanel = () => {
+		panelOpen = false;
+	};
+</script>
+
+<div class={s.end.end}>
+	<button on:click={togglePanel} class={s.end.menu}>
+		MENÚ
+		<svg xmlns="http://www.w3.org/2000/svg" class={s.end.svg} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+			<path stroke-linecap="round" stroke-linejoin="round"stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+		</svg>
+	</button>
+</div>
+
+<div class={s.end.panelLateral} class:translate-x-full={!panelOpen}>
+	<button on:click={closePanel} class={s.end.menu}>
+		MENÚ
+		<svg xmlns="http://www.w3.org/2000/svg" class={s.end.svg} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
+		</svg>
+	</button>
+	<a href="/solicitud" class={s.end.a}>SIMULADOR DE <br/> CRÉDITO</a>
+</div>
+
+<div 
+	class={s.end.contenedorInvisible}
+	class:hidden={!panelOpen}
+	on:click={closePanel}
+	role="button"
+	tabindex="0"
+	on:keydown={(e) => e.key === 'Enter' || e.key === ' ' ? closePanel() : null}
+	aria-label="Cerrar panel">
+</div>
--- web/src/components/header/styles.ts
+++ web/src/components/header/styles.ts
@@ -1,18 +1,26 @@
-const Nav:string ="w-full max-w-full bg-black px-8 py-6 flex justify-between items-center h-auto";
-const NavDiv1:string = "flex items-center";
-const NavDivA:string = "h-auto w-[10%] translate-x-[50%]";
-const NavDivASVG:string = "max-w-[100%] h-auto";
-const NavDiv2:string = "flex items-center justify-between gap-4 translate-x-[-50%]";
-const NavDiv2A:string = "h-auto w-[100%]";
-const NavDiv2ASVG:string = "max-w-[100%] h-auto";
+const Nav:string ="navbar bg-base-100 border-b border-b-black";
+const start:string = "navbar-start";
+const center:string = "navbar-center";
+const enlacePaginaPrincipal:string = "h-auto w-auto flex items-center";
+const end:string = "navbar-end";
+const menu:string = "btn btn-ghost text-xs uppercase tracking-wider";
+const svg:string = "ml-2 h-4 w-4";
+const panelLateral:string = "fixed top-0 right-0 h-screen w-40 bg-black text-white rounded-l-[40px] shadow-lg transform transition-transform duration-300 z-50 flex flex-col items-end p-2 space-y-5";
+const a:string = "text-[0.9rem] p-2 hover:underline";
+const contenedorInvisible:string = "fixed inset-0 bg-transparent bg-opacity-50 z-40";
 
 const styles = {
- Nav,
- NavDiv1,
- NavDivA,
- NavDivASVG,
- NavDiv2,
- NavDiv2A,
- NavDiv2ASVG
+	Nav,
+	start,
+	center,
+	enlacePaginaPrincipal,
+	end:{
+		end,
+		menu,
+		svg,
+		panelLateral,
+		a,
+		contenedorInvisible
+	}
 };
 export default styles;
