ef4232722511d943ad332369f3efdffea6b27304
Sebastian Asprino <sebastian@asprino.dev>
2025-04-09 08:30:10 -0500

    refactorizado estilos mainpage

--- web/src/components/pagina-principal/styles.ts
+++ web/src/components/pagina-principal/styles.ts
@@ -1,18 +1,22 @@
-const main:string = "flex h-screen overflow-hidden";
-const svg:string = "absolute top-10 left-10 w-[6%] h-auto";
-const SectionIzquierdo:string = "w-1/2 h-full flex flex-col justify-center items-center relative overflow-hidden bg-black text-white p-[5%] box-border";
-const SectionIzquierdoH1:string ="text-[6vw] mb-[2%] leading-[1.3] mt-[-9%] font-bold";
-const SectionIzquierdoP:string ="text-[1.5vw] mb-[8%]";
-const SectionIzquierdoA:string ="bg-white text-black py-[2%] px-[5%] no-underline rounded text-[2vw] transition-all duration-300 hover:scale-110 hover:shadow-lg hover:brightness-90 hover:bg-gray-200";
-const SectionDerecho:string ="w-1/2 h-full bg-white relative";
+const main:string = "flex flex-col sm:flex-row h-screen overflow-hidden";
+const sectionIzquierdo:string = "w-full sm:w-1/2 h-full flex flex-col justify-start items-start relative overflow-hidden text-black p-1 box-border order-2 sm:order-1";
+const h1:string = "text-[7vw] sm:text-[3.5vw]  leading-[1.3] mt-[6vw] sm:mt-[4vw] ml-[5.7vw]  mb-0 font-bold";
+const p:string = "text-[2.96vw] sm:text-[1.47vw] m-5 ml-[5.7vw] font-semibold";
+const contenedorPs:string = "flex flex-row items-center gap-2 sm:gap-1 m-5 ml-[5.7vw]";
+const ps:string = "bg-white text-black border border-black py-[1%] px-[6%] rounded-xl text-[2.5vw] sm:text-[1.1vw]";
+const sectionDerecho:string = "w-full sm:w-1/2 h-full bg-white relative order-1 md:order-2";
+
+
+
+
 const styles = {
 	main,
-	svg,
-	SectionIzquierdo,
-	SectionIzquierdoH1,
-	SectionIzquierdoP,
-	SectionIzquierdoA,
-	SectionDerecho
+	sectionIzquierdo,
+	h1,
+	p,
+	contenedorPs,
+	ps,
+	sectionDerecho
 };
 
 export default styles;
